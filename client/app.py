0a3c9c50963fb200a0e99b058661ee828232cf92
millenia911 <freddymyusuf@gmail.com>
2022-08-16 07:22:20 +0700

    create submit image event handler

--- client/app.py
+++ client/app.py
@@ -1,3 +1,44 @@
 import socketio
+import cv2
+import os
+import tqdm
+from multiprocessing import Process
 
-sio = socketio.AsyncClient()
+sio = socketio.Client()
+
+def convert_picture(path):
+    img = cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    _, img = cv2.imencode('.jpg', img)
+    return img.tobytes()    
+@sio.event
+def connect():
+    print('connection established')
+@sio.event
+def disconnect():
+    print('disconnected from server')
+    exit()
+    
+def emit_picture_submit(name, path):
+    img_byte = convert_picture(path)
+    sio.emit("submit_picture", {"name":name, "image_bytes": img_byte}, namespace="/admin")
+
+def main_process():
+    sio.connect("http://127.0.0.1:5000", wait_timeout=200, namespaces=["/", "/admin"])
+    print("SID is ", sio.sid)
+    for f in os.listdir("./people"):
+        pth = os.path.join("./people", f)
+        if os.path.isdir(pth):
+            pbar = tqdm.tqdm(total=len(os.listdir(pth)))
+            print(f"Sumbitting images for {f}")
+            for p in os.listdir(pth):
+                emit_picture_submit(name=f, path=os.path.join(pth, p))
+                pbar.update(1)
+            pbar.close()
+    sio.emit("disconnect_request", namespace="/")  
+    sio.emit("disconnect_request", namespace="/admin")
+
+p1 = Process(target=main_process)
+p1.start()
+p1.join()
+
--- server/app.py
+++ server/app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, copy_current_request_context
 from flask_socketio import SocketIO, emit, disconnect, Namespace
+import multiprocessing as mp
 import numpy as np
 import cv2
 import os
 import shutil
 import uuid
+import random
+import asyncio
 
-app = Flask(__name__,)
+app = Flask(__name__)
 app.config["SECRET_KEY"] = str(uuid.uuid4())
 app.debug = True
-socketio = SocketIO(app, async_mode=None)
+socketio = SocketIO(app, async_mode="eventlet", logger=True, 
+                    engineio_logger=True, max_http_buffer_size=100000000)
 
 class AdminPage(Namespace):
     def __init__(self, namespace, im_path="./people"):
@@ -29,19 +33,26 @@ class AdminPage(Namespace):
         else:
             emit("admin_event_message", {"status":"failed",
                                          "message":"sorry, something is wrong"})
-    @socketio.event
-    def on_submit_picture(self, name, image_bytes):
+    # @socketio.event
+    def on_submit_picture(self, data):
+        name = data["name"]
+        image_bytes = data["image_bytes"]
+        print(name, "is retrieved")
+        print(type(image_bytes))
         if not self.is_valid_data([name, image_bytes]):
+            print("failed?")
             self.emit_admin_event_message(status="failed", 
-                                          msg="Name or image cannot be none or zero lenght")
+                                            msg="Name or image cannot be none or zero lenght")
             return
+        print("here")
         nparr = np.frombuffer(image_bytes, np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        pic_path = os.path.join(self.im_path)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        pic_path = os.path.join(self.im_path, name)
         os.makedirs(pic_path, exist_ok=True)
-        _ = cv2.imwrite(pic_path, img)
+        _ = cv2.imwrite(os.path.join(pic_path, f"{name}_{random.randint(1, 100000)}.jpg"), img)
 
-    @socketio.event
+    # @socketio.event
     def on_delete_person(self, name):
         # TODO: delete on embedding pickle and restart emb dataframe on inference
         if not self.is_valid_data([name]):
@@ -54,8 +65,26 @@ class AdminPage(Namespace):
         else:
             self.emit_admin_event_message(status="failed",
                                           msg="Name is not found")
+    def on_disconnect_request():
+        @copy_current_request_context
+        def dc():
+            disconnect()
+        dc()
+    
 
-socketio.on_namespace(AdminPage("/api/admin"))
-        
+
+socketio.on_namespace(AdminPage("/admin"))
+
+@socketio.event
+def disconnect_request():
+    @copy_current_request_context
+    def dc():
+        disconnect()
+    dc()
+    
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected', request.sid)
+
 if __name__ == "__main__":
     socketio.run(app, port=5000)
