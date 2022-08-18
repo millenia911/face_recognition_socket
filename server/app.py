de9e7016970f0a72062896479b9472dc36511e1b
millenia911 <freddymyusuf@gmail.com>
2022-08-19 01:46:31 +0700

    feat: add alternative `POST` method to register new faces

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, copy_current_request_context
+from flask import Flask, request, copy_current_request_context, Response
 from flask_socketio import SocketIO, disconnect
 from sections.admin_page import AdminPage
 from sections.stream_page import StreamPage
-import uuid
+import uuid, cv2, os, random, json, base64
+from io import BytesIO
+import numpy as np
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = str(uuid.uuid4())
@@ -13,6 +15,25 @@ socketio = SocketIO(app, async_mode="eventlet", logger=True,
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/admin", methods=["POST"])
+def submit_picture_request():
+    try:
+        req = request.json
+        req = json.loads(req)
+        name = req["name"]
+        image_bytes = req["image_bytes"]
+        image_bytes = list(image_bytes.values())[0]
+        image_bytes = base64.b64decode(image_bytes)
+        im_path = "./people"
+        nparr = np.frombuffer(image_bytes, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        pic_path = os.path.join(im_path, name)
+        os.makedirs(pic_path, exist_ok=True)
+        _ = cv2.imwrite(os.path.join(pic_path, f"{name}_{random.randint(1, 100000)}.jpg"), img)
+        return Response(response="success", status=200, mimetype="text/plain")
+    except Exception as e:
+        return Response(response=str(e), status=400, mimetype="text/plain")
 
 socketio.on_namespace(AdminPage("/admin"))
 socketio.on_namespace(StreamPage("/stream"))
