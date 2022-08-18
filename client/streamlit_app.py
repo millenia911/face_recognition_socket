82cfbccc245f0b0bcf4cff089d635f7e66352bdd
millenia911 <freddymyusuf@gmail.com>
2022-08-19 01:47:52 +0700

    feat: integrate image upload via `POST` method

--- client/streamlit_app.py
+++ client/streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
+import socketio
+import requests
+import jsonpickle
 from streamlit import session_state as sess
 
+sio = socketio.Client()
+
 def init_session_states():
-    state_list = ["role"]
+    state_list = ["role", "socket_connected"]
     for _state in state_list:
         if _state not in sess:
             sess[_state] = None
@@ -20,14 +25,14 @@ def admin_page():
     with st.form("submit_form"):
         name = st.text_input("Input name")
         files = st.file_uploader("Choose images", accept_multiple_files=True, type=["jpg", "png", "jpeg"])
-        img_file_buffer = st.camera_input("Take a picture")
+        # img_file_buffer = st.camera_input("Take a picture")
         sub = st.form_submit_button("Submit")
         if sub:
             if len(files) > 0 and len(name) > 3:
                 pbar = st.progress(0)
                 num = (int(100/len(files)))
                 for n, f in enumerate(files):
-                    data_bytes = f.read()
+                    data_bytes = f.getvalue()
                     submit_image(name, data_bytes)
                     pbar.progress((n+1)*num)
                 st.info(F"Pictures for {name} is submited!")
@@ -37,13 +42,29 @@ def admin_page():
 def streamer_page():
     pass
 
-def submit_image(name, data):
-    print(name)
+def submit_image(name, img_bytes:bytes):
+    print("sending picture for", name)
+    req = {"name":name, 
+            "image_bytes":img_bytes}
+    res = requests.post("http://127.0.0.1:8888/api/admin", 
+                        json=jsonpickle.encode(req))
+    return res.text, res.status_code
 
+def connect_socketio():
+    print("connecting to socket server...")
+    print("please wait")
+    sio.connect("http://127.0.0.1:8888", 
+                    wait_timeout=200, 
+                    namespaces=["/admin", "/streamer"])
+    print("SID is ", sio.sid)
 
 
 if __name__ == "__main__":
     init_session_states()
+    # connect_socketio()
+    if sess["socket_connected"]:
+        st.text("connected to socketio server")
+    # else: connect_socketio()
     if sess["role"] is None:
         login_page()
 
